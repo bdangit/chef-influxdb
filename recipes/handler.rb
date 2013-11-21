bc6f96c306b57939f72d326f18dab569084b0dd2
Jake Davis <jake.davis5989@gmail.com>
2013-11-21 00:33:29 +0000

    Add support for the ChefInfluxDB handler
    Adds a dependency on chef_handler and adds a new handler recipe to install the
    chef-handler-influxdb gem and setup a report handler.

--- recipes/handler.rb
+++ recipes/handler.rb
@@ -1,6 +1,8 @@
+# recipes/handler.rb
+#
 # Author: Simple Finance <ops@simple.com>
 # License: Apache License, Version 2.0
-#
+# 
 # Copyright 2013 Simple Finance Technology Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,19 +16,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Installs the ChefInfluxDB report handler
 
-name             'influxdb'
-maintainer       'Simple Finance Technology Corp'
-maintainer_email 'ops@simple.com'
-license          'Apache 2.0'
-description      'InfluxDB, a timeseries database'
-version          '1.0.0'
+include_recipe 'chef_handler::default'
 
-# For CLI client
-# https://github.com/balbeko/chef-npm
-suggests 'npm'
+chef_gem 'chef-handler-influxdb' do
+  version '0.1.4'
+  action :nothing
+end.run_action(:install)
 
-# For ChefInfluxDB Chef handler 
-# https://github.com/jakedavis/chef-handler-influxdb
-depends 'chef_handler'
+# Since arguments are required for this Chef handler, you can do the following
+# in another cookbook to ensure this works :
+# resources('chef_handler[ChefInfluxDB]').arguments[ 
+#   :database => 'test',
+#   :series => 'mine'
+# ]
+chef_handler 'ChefInfluxDB' do
+  source ::File.join(Gem::Specification.find_by_name('chef-handler-influxdb').lib_dirs_glob,
+                     'chef-handler-influxdb.rb')
+  action :nothing
+end.run_action(:enable)
 
