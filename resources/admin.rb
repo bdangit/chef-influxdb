b091305aeb67e7a7e8710ee33674746acef12d3c
Jake Davis <jake.davis5989@gmail.com>
2013-11-22 18:19:06 +0000

    [1.3.0] Allow creation, updating, and deletion of cluster admins
    * Adds new influxdb_admin LWRP

--- resources/admin.rb
+++ resources/admin.rb
@@ -1,6 +1,8 @@
+# resources/admin.rb
+#
 # Author: Simple Finance <ops@simple.com>
 # License: Apache License, Version 2.0
-#
+# 
 # Copyright 2013 Simple Finance Technology Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,19 +16,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# LWRP for InfluxDB cluster admin
 
-name             'influxdb'
-maintainer       'Simple Finance Technology Corp'
-maintainer_email 'ops@simple.com'
-license          'Apache 2.0'
-description      'InfluxDB, a timeseries database'
-version          '1.3.0'
+actions(:create, :update, :delete)
+default_action(:create)
 
-# For CLI client
-# https://github.com/balbeko/chef-npm
-suggests 'npm'
-
-# For ChefInfluxDB Chef handler 
-# https://github.com/jakedavis/chef-handler-influxdb
-depends 'chef_handler'
+attribute(:username, :kind_of => String, :name_attribute => true)
+attribute(:password, :kind_of => String, :required => true)
 
