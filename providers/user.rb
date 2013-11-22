ab96713bf69dc055fe1487f3865c1a23e198cf3a
Jake Davis <jake.davis5989@gmail.com>
2013-11-22 18:53:15 +0000

    Use 'client' over 'cli' in providers

--- providers/user.rb
+++ providers/user.rb
@@ -23,7 +23,7 @@ include InfluxDB::Helpers
 
 def initialize(new_resource, run_context)
   super
-  @cli       = InfluxDB::Helpers.client('root', 'root')
+  @client    = InfluxDB::Helpers.client('root', 'root')
   @username  = new_resource.username
   @password  = new_resource.password
   @databases = new_resource.databases
@@ -31,7 +31,7 @@ end
 
 action :create do
   @databases.each do |db|
-    @cli.create_database_user(db, @username, @password)
+    @client.create_database_user(db, @username, @password)
   end
 end
 
