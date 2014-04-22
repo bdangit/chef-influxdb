b9834128eb92cbc2031796ebc1cf8b76f17f21bb
Miah Johnson <miah@chia-pet.org>
2014-04-22 12:02:05 -0700

    ruby 1.9 hash.

--- resources/default.rb
+++ resources/default.rb
@@ -22,7 +22,7 @@
 actions(:create, :start, :delete)
 default_action(:create)
 
-attribute(:name, :kind_of => String, :name_attribute => true)
-attribute(:source, :kind_of => String, :required => true)
-attribute(:checksum, :kind_of => String, :required => false)
-attribute(:config, :kind_of => Hash, :required => false)
+attribute(:name, kind_of: String, name_attribute: true)
+attribute(:source, kind_of: String, required: true)
+attribute(:checksum, kind_of: String, required: false)
+attribute(:config, kind_of: Hash, required: false)
