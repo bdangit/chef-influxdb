0f9970b9d8be7373e4f1a4d8b9e7fb0cb041fa46
Miah Johnson <miah@chia-pet.org>
2014-04-22 12:00:38 -0700

    ruby 1.9 hash.

--- resources/admin.rb
+++ resources/admin.rb
@@ -22,5 +22,5 @@
 actions(:create, :update, :delete)
 default_action(:create)
 
-attribute(:username, :kind_of => String, :name_attribute => true)
-attribute(:password, :kind_of => String)
+attribute(:username, kind_of: String, name_attribute: true)
+attribute(:password, kind_of: String)
