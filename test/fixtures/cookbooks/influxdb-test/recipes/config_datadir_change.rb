60a10e60e53431a6ab1577590f5811f1f07cf7d9
Chris Duong <chris.duong83@gmail.com>
2016-02-25 11:27:16 +0700

    Restate these for consistent way with upstream cookbook

--- test/fixtures/cookbooks/influxdb-test/recipes/config_datadir_change.rb
+++ test/fixtures/cookbooks/influxdb-test/recipes/config_datadir_change.rb
@@ -6,8 +6,16 @@
 # Mock mount disk0
 directory '/mnt/disk0'
 
-# override default `lib_file_path` by restate nested attributes
+# New lib_file_path
 node.default['influxdb']['lib_file_path'] = '/mnt/disk0/influxdb'
+
+# Restate these for consistent way with upstream cookbook
+node.default['influxdb']['meta_file_path'] = "#{node['influxdb']['lib_file_path']}/meta"
+node.default['influxdb']['data_file_path'] = "#{node['influxdb']['lib_file_path']}/data"
+node.default['influxdb']['wal_file_path'] = "#{node['influxdb']['lib_file_path']}/wal"
+node.default['influxdb']['hinted-handoff_file_path'] = "#{node['influxdb']['lib_file_path']}/hh"
+
+# override by restating nested attributes
 node.default['influxdb']['config']['meta']['dir'] = "#{node['influxdb']['lib_file_path']}/meta"
 node.default['influxdb']['config']['data']['dir'] = "#{node['influxdb']['lib_file_path']}/data"
 node.default['influxdb']['config']['data']['wal-dir'] = "#{node['influxdb']['lib_file_path']}/wal"
