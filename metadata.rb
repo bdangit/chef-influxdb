75e04605cf1fca0f43f095018a257916f8cc8030
Ben Dang <me@bdang.it>
2016-06-02 09:07:17 -0700

    update CHANGELOG.md and bump version

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer       'Ben Dang'
 maintainer_email 'me@bdang.it'
 license          'MIT'
 description      'InfluxDB, a timeseries database'
-version          '4.2.0'
+version          '4.3.0'
 
 # For CLI client
 # https://github.com/redguide/nodejs
