e58737da83586846caf815dc38b538d79469bd82
Matt Clyde <matt.clyde@oracle.com>
2018-04-13 15:45:25 -0700

    Modify custom resource definitions for Chef 14 compatibility

--- resources/retention_policy.rb
+++ resources/retention_policy.rb
@@ -18,49 +18,39 @@ property :verify_ssl, [TrueClass, FalseClass], default: true
 
 default_action :create
 
+include InfluxdbCookbook::Helpers
+
 action :create do
-  if current_policy
-    if current_policy['duration'] != duration || current_policy['replicaN'] != replication || current_policy['default'] != default
-      client.alter_retention_policy(policy_name, database, duration, replication, default)
-      updated_by_last_action true
+  client = client(new_resource)
+
+  if current_policy(client)
+    if @current_policy['duration'] != new_resource.duration || @current_policy['replicaN'] != new_resource.replication || @current_policy['default'] != new_resource.default
+      client.alter_retention_policy(new_resource.policy_name, new_resource.database, new_resource.duration, new_resource.replication, new_resource.default)
     end
   else
-    client.create_retention_policy(policy_name, database, duration, replication, default)
-    updated_by_last_action true
+    client.create_retention_policy(new_resource.policy_name, new_resource.database, new_resource.duration, new_resource.replication, new_resource.default)
   end
 end
 
 action :delete do
-  if current_policy
-    client.delete_retention_policy(policy_name, database)
-    updated_by_last_action true
+  client = client(new_resource)
+
+  if current_policy(client)
+    client.delete_retention_policy(new_resource.policy_name, new_resource.database)
   end
 end
 
-def current_policy
-  @current_policy ||= begin
-    current_policy_arr = client.list_retention_policies(database).select do |p|
-      p['name'] == policy_name
+action_class.class_eval do
+  def current_policy(client)
+    @current_policy ||= begin
+      current_policy_arr = client.list_retention_policies(new_resource.database).select do |p|
+        p['name'] == new_resource.policy_name
+      end
+      if current_policy_arr.length > 1
+        Chef::Log.fatal("Unexpected number of matches for retention policy #{new_resource.policy_name} on database #{new_resource.database}: #{current_policy_arr}")
+      end
+      current_policy_arr[0] if current_policy_arr.length
     end
-    if current_policy_arr.length > 1
-      Chef::Log.fatal("Unexpected number of matches for retention policy #{policy_name} on database #{database}: #{current_policy_arr}")
-    end
-    current_policy_arr[0] if current_policy_arr.length
   end
 end
 
-# rubocop:disable Metrics/MethodLength
-def client
-  require 'influxdb'
-  @client ||=
-    InfluxDB::Client.new(
-      username: auth_username,
-      password: auth_password,
-      retry: retry_limit,
-      host: api_hostname,
-      port: api_port,
-      use_ssl: use_ssl,
-      verify_ssl: verify_ssl
-    )
-end
-# rubocop:enable Metrics/MethodLength
